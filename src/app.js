1921986b9d205f320d0a0a5b91ae2dd98ae598d5
Jorge Grijalva <georgegrijalva75@gmail.com>
2025-06-13 12:01:32 -0600

    refactor: add new CORS origin for audio extraction endpoint and standardize file size limit definition

--- src/app.js
+++ src/app.js
@@ -36,7 +36,8 @@ const corsOptions = {
       "http://debor-ai-api-4rremg-ce3bf2-31-220-17-216.traefik.me:3000",
       "https://ffmpeg.debor.ai",
       "http://localhost:3000",
-      "https://f2g2326j-3000.usw3.devtunnels.ms"
+      "https://f2g2326j-3000.usw3.devtunnels.ms",
+      "https://ffmpeg.debor.ai/video/extract/audio?mono=no"
     ];
     // Permitir solicitudes sin origen (como aplicaciones móviles o curl)
     if (!origin) return callback(null, true);
