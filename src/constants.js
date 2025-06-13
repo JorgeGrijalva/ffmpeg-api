47fb2a73eba005a93dd6a7b864fd6438918f9726
Jorge Grijalva <georgegrijalva75@gmail.com>
2025-06-13 11:47:55 -0600

    refactor: update file size limit to 5GB for improved upload capacity

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 
-exports.fileSizeLimit = parseInt(process.env.FILE_SIZE_LIMIT_BYTES || "536870912"); //536870912 = 512MB 
+exports.fileSizeLimit = parseInt(process.env.FILE_SIZE_LIMIT_BYTES || 5e9); //5368709120 = 5GB
 exports.defaultFFMPEGProcessPriority=10;
 exports.serverPort = 3000;//port to listen, NOTE: if using Docker/Kubernetes this port may not be the one clients are using
 exports.externalPort = process.env.EXTERNAL_PORT;//external port that server listens, set this if using for example docker container and binding port is other than 3000
